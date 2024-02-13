b27446d6925c4de1ef487e0994719791fb09ed60
Anton <35538867+Anton13602@users.noreply.github.com>
2024-02-14 01:06:24 +0300

    feat: add 500 error

--- src/index.ts
+++ src/index.ts
@@ -7,58 +7,61 @@ const PORT = process.env.NODE_TEST ? 5000 : (process.env.PORT || defaultPort);
 
 export const startServer = async () => {
   const server = createServer((request, response) => {
-    const { url, method } = request;
-    const propsUrl = request.url?.split('/')[3] ?? '';
-    let data: string = '';
+    try {
+      const { url, method } = request;
+      const propsUrl = request.url?.split('/')[3] ?? '';
+      let data: string = '';
 
-    console.log(url);
+      if (url?.slice(0, defaultRout.length) !== defaultRout) {
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({
+          message: `${url} - not found.`,
+        }));
+      }
 
-    if (url?.slice(0, defaultRout.length) !== defaultRout) {
-      response.writeHead(404, { 'Content-Type': 'application/json' });
+      request.on('data', (chunk) => {
+        data += chunk.toString();
+      }).on('end', async () => {
+        let status: number = 500;
+        let header: string = '';
+        let body: string = '';
+
+        if (method === RestMethod.POST && data.length) {
+          ({ status, header, body } = await addUser(data));
+        }
+
+        if (method === RestMethod.GET && propsUrl.length) {
+          ({ status, header, body } = await getUserById(propsUrl));
+        }
+
+        if (method === RestMethod.GET && !propsUrl.length) {
+          ({ status, header, body } = await getAllUsers());
+        }
+
+        if (method === RestMethod.PUT && data.length && propsUrl.length) {
+          const props = {
+            id: propsUrl,
+            data: data,
+          };
+
+          ({ status, header, body } = await updateUser(props));
+        }
+
+        if (method === RestMethod.DELETE && propsUrl.length) {
+          ({ status, header, body } = await deleteUser(propsUrl));
+        }
+
+        response.writeHead(status, header);
+        response.end(body);
+      });
+    } catch (e) {
+      response.writeHead(500, 'Server error');
       response.end(JSON.stringify({
-        message: `${url} - not found.`,
+        message: 'Server error.',
       }));
     }
-
-    request.on('data', (chunk) => {
-      data += chunk.toString();
-    }).on('end', async () => {
-      let status: number = 500;
-      let header: string = '';
-      let body: string = '';
-
-      if (method === RestMethod.POST && data.length) {
-        ({ status, header, body } = await addUser(data));
-      }
-
-      if (method === RestMethod.GET && propsUrl.length) {
-        ({ status, header, body } = await getUserById(propsUrl));
-      }
-
-      if (method === RestMethod.GET && !propsUrl.length) {
-        ({ status, header, body } = await getAllUsers());
-      }
-
-      if (method === RestMethod.PUT && data.length && propsUrl.length) {
-        const props = {
-          id: propsUrl,
-          data: data,
-        };
-
-        ({ status, header, body } = await updateUser(props));
-      }
-
-      if (method === RestMethod.DELETE && propsUrl.length) {
-        ({ status, header, body } = await deleteUser(propsUrl));
-      }
-
-      console.log(propsUrl);
-
-
-      response.writeHead(status, header);
-      response.end(body);
-    });
   });
+
   server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
   });
